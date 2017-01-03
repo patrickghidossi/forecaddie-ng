5db81912f48270dfdb54e7a631d9eb03e7dd82d4
patrickghidossi <patrick.ghidossi@gmail.com>
2017-01-03 15:22:23 -0700

    Inital Setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { LandComponent } from './land/land.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandComponent,
   ],
   imports: [
     BrowserModule,
